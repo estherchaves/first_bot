f7c7cd7310a04d2272cbfe591c40965a09ca3fc7
Esther Chaves Coelho <echaves122@gmail.com>
2025-04-13 17:10:16 +0100

    update db.js

--- bot_backend/db.js
+++ bot_backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '@Violettateen2004',
+  password: '',
   database: 'stock_control'
 });
 
